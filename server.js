16d172f48d98dcff0f2ec00c78b03404e7ff9f23
James Trebilcock <james.trebilcock@stash.com>
2022-02-20 22:38:40 -0500

    node app

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send('Hiyo')
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
+
+
