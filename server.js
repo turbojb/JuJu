b99bc8af6a2c70b8a664e9b7b8e2728fb1ad5bbb
James Trebilcock <james.trebilcock@stash.com>
2022-02-21 22:40:42 -0500

    build app out

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express   = require('express')
+const basicAuth = require('express-basic-auth')
+const path      = require('path')
+const app       = express()
+const port      = process.env.PORT || 8080
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(
+    basicAuth({
+        challenge: true,
+        users: { 'admin': process.env.SECRET }
+    })
+)
 
 app.get('/', (req, res) => {
-    res.send('Hiyo')
+    res.render(path.join(__dirname, 'views/index'), {
+        clients: []
+    });
+})
+
+app.get('/rocco', (req, res) => {
+    res.render(path.join(__dirname, 'views/rocco'), {
+        clients: []
+    });
 })
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+    console.log(`**** Server listening on port ${port} ****`)
 });
-
-
